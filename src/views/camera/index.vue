dd3860124a3fb1b06d6228919f2af718b6d4781a
XwenHaHa <929716663@qq.com>
2024-10-24 15:43:11 +0800

    feat:新增实时获取照片对比

--- src/views/camera/index.vue
+++ src/views/camera/index.vue
@@ -1,25 +1,68 @@
 <template>
-  <div>
-    <el-button @click="handlePhoto" type="primary">点击拍照</el-button>
-    <el-button @click="handleClear">清空图片</el-button>
-    <el-button @click="handleDetectFace">对比图片</el-button>
-    <div>
-      <el-image
-        style="width: 200px; height: 200px"
-        :src="imageURL"
-        fit="contain"
-      />
+  <div class="camera-page">
+    <div class="item">
+      <div class="header">
+        <el-button @click="handlePhoto" type="primary">点击拍照</el-button>
+        <el-button @click="handleClear">清空图片</el-button>
+        <el-upload :on-change="handleChangeImage" list-type="picture">
+          <el-button type="primary">上传本地图片</el-button>
+        </el-upload>
+        <el-button @click="handleCompare">对比图片</el-button>
+      </div>
+      <div class="container">
+        <div>
+          <el-image
+            style="width: 200px; height: 200px"
+            :src="imageURL"
+            fit="contain"
+          />
+          <div class="text">拍照后图片</div>
+        </div>
+        <div>
+          <el-image
+            style="width: 200px; height: 200px"
+            :src="localImageURL"
+            fit="contain"
+          />
+          <div class="text">本地图片</div>
+        </div>
+      </div>
     </div>
-    <div>
-      <el-upload :on-change="handleChangeImage" list-type="picture">
-        <el-button type="primary">上传本地图片</el-button>
-      </el-upload>
-      <el-image
-        style="width: 200px; height: 200px"
-        :src="localImageURL"
-        fit="contain"
-      />
+    <div class="item">
+      <div class="header">
+        <el-button type="primary" @click="handleInvokeCamera">
+          点击调起取景框
+        </el-button>
+        <el-button type="primary" @click="handleCloseCamera">
+          点击还原
+        </el-button>
+        <el-button type="primary" @click="handleRealTimeShot">
+          实景拍摄
+        </el-button>
+        <el-upload :on-change="handleChangeRealTimeImage" list-type="picture">
+          <el-button type="primary">上传对比图</el-button>
+        </el-upload>
+        <el-button @click="handleRealTimeCompare"> 实景对比 </el-button>
+      </div>
+      <div class="container">
+        <video ref="videoItem" class="video-item" v-show="showVideo"></video>
+        <canvas
+          ref="videoCanvas"
+          width="500"
+          height="346"
+          v-show="showCanvas"
+        ></canvas>
+        <div>
+          <el-image
+            style="width: 200px; height: 200px"
+            :src="localRealTimeImageURL"
+            fit="contain"
+          />
+          <div class="text">本地图片</div>
+        </div>
+      </div>
     </div>
+    <div class="item">111</div>
   </div>
 </template>
 
@@ -104,20 +147,20 @@ const compressImage = (file) => {
   });
 };
 
-const handleDetectFace = async () => {
-  if (imageURL.value && localImageURL.value) {
+const handleDetectFace = async (imageURL, localImageURL) => {
+  if (imageURL && localImageURL) {
     const formData = new FormData();
     formData.append("api_key", "1VKcCS0B3QUMFsHb022mxq9WdpJvlAqL");
     formData.append("api_secret", "YTx16Bl5ZE0PqVOktyQIy24lPgzItLUA");
 
     // 压缩第一张图片
-    const response1 = await fetch(imageURL.value);
+    const response1 = await fetch(imageURL);
     const blob1 = await response1.blob();
     const base64Image1 = await compressImage(blob1);
     formData.append("image_base64_1", base64Image1);
 
     // 压缩第二张图片
-    const response2 = await fetch(localImageURL.value);
+    const response2 = await fetch(localImageURL);
     const blob2 = await response2.blob();
     const base64Image2 = await compressImage(blob2);
     formData.append("image_base64_2", base64Image2);
@@ -132,6 +175,13 @@ const handleDetectFace = async () => {
           },
         }
       );
+      if (!response.data.confidence) {
+        ElMessage({
+          message: `没有可比性！`,
+          type: "error",
+        });
+        return;
+      }
       if (response.data.confidence) {
         ElMessage({
           message: `相似度为${response.data.confidence}%`,
@@ -145,10 +195,114 @@ const handleDetectFace = async () => {
     console.log("请先上传图片");
   }
 };
+
+const handleCompare = () => {
+  handleDetectFace(imageURL.value, localImageURL.value);
+};
+
 const handleChangeImage = async (file) => {
   const base64Image = await compressImage(file.raw);
   localImageURL.value = base64Image; // 保存 Base64 字符串
 };
+
+// 调起取景框
+const videoItem = ref({});
+let mediaStream = null; // 用于存储媒体流
+const readSuccess = (stream) => {
+  // 保存媒体流以便后续销毁
+  mediaStream = stream;
+  /* 将stream 分配给video标签 */
+  videoItem.value.srcObject = stream;
+  videoItem.value.play();
+};
+
+const showVideo = ref(false);
+const showCanvas = ref(false);
+const handleInvokeCamera = () => {
+  showVideo.value = true;
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (stream) {
+      /* 使用这个 stream 传递到成功回调中 */
+      readSuccess(stream);
+    })
+    .catch(function (err) {
+      /* 处理 error 信息 */
+      console.error(err);
+    });
+};
+
+const handleCloseCamera = () => {
+  showVideo.value = false;
+  showCanvas.value = false;
+  localRealTimeImageURL.value = "";
+  if (mediaStream) {
+    mediaStream.getTracks().forEach((track) => {
+      track.stop();
+    });
+  }
+  // 重置 video 元素的源
+  if (videoItem.value) {
+    videoItem.value.srcObject = null;
+  }
+  // 可选：将 mediaStream 设置为 null，以便垃圾回收
+  mediaStream = null;
+};
+
+const videoCanvas = ref(null);
+const handleRealTimeShot = async () => {
+  showCanvas.value = true;
+  const context = videoCanvas.value.getContext("2d");
+  // 使用 drawImage 将视频帧绘制到 Canvas
+  context.drawImage(videoItem.value, 0, 0, 1000, 692, 0, 0, 500, 346);
+  showVideo.value = false;
+};
+
+const localRealTimeImageURL = ref("");
+const handleChangeRealTimeImage = async (file) => {
+  const base64Image = await compressImage(file.raw);
+  localRealTimeImageURL.value = base64Image; // 保存 Base64 字符串
+};
+
+const handleRealTimeCompare = async () => {
+  const base64Url = videoCanvas.value.toDataURL();
+  await handleDetectFace(base64Url, localRealTimeImageURL.value);
+};
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.camera-page {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  .item {
+    width: 48%;
+    height: 48%;
+    background-color: #ccc;
+    .header {
+      display: flex;
+      :nth-last-child(2) {
+        margin: 0 5px;
+      }
+    }
+    .container {
+      display: flex;
+      width: 100%;
+      > div {
+        margin: 0 10px;
+      }
+    }
+    .video-item {
+      width: 200px;
+      height: 200px;
+    }
+  }
+  .line {
+    width: 1px;
+    height: 100%;
+    background-color: #ccc;
+  }
+}
+</style>
